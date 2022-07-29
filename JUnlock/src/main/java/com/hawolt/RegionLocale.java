7835eab04066e20562624a287b077180092fa0f1
hawolt <hawolton@gmail.com>
2022-07-29 04:50:44 +0200

    apparently this is supposed to fix garena (not tested)

--- JUnlock/src/main/java/com/hawolt/RegionLocale.java
+++ JUnlock/src/main/java/com/hawolt/RegionLocale.java
@@ -14,7 +14,8 @@ public class RegionLocale {
         this.locale = object.getString("locale");
         this.region = object.getString("region");
         this.webLanguage = object.getString("webLanguage");
-        this.webRegion = object.getString("webRegion");
+        String webRegion = object.getString("webRegion");
+        this.webRegion = (webRegion == null || webRegion.length() == 0) ? this.region : webRegion;
     }
 
     public String getLocale() {
